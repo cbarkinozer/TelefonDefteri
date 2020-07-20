68c972bb5f3a5acca92214a035b8a1ab645961d3
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-20 14:49:43 +0300

    Update summing.vb

--- summing.vb
+++ summing.vb
@@ -4,24 +4,29 @@ Public Class Form1
     Dim sayi3 As Integer
 
     Private Sub Topla_Click(sender As Object, e As EventArgs) Handles Topla.Click
-        Try
-            sayi3 = sayi1 + sayi2
-            TextBox3.Text = sayi3
-        Catch tipHata As System.InvalidCastException
-            MsgBox("Lütfen tamsayı girin")
-        Catch hata As Exception
-            MsgBox("Bilinmeyen hata")
-        End Try
+        sayi3 = sayi1 + sayi2
+        TextBox3.Text = sayi3
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-        sayi1 = CInt(TextBox1.Text)
+        Try
+            sayi1 = CInt(TextBox1.Text)
+        Catch tipHata As System.InvalidCastException
+            MsgBox("Lütfen tamsayı girin. " & tipHata.Message)
+        Catch hata As Exception
+            MsgBox("Bilinmeyen hata. " & hata.Message)
+        End Try
     End Sub
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Try
+            sayi2 = CInt(TextBox2.Text)
+        Catch tipHata As System.InvalidCastException
+            MsgBox("Lütfen tamsayı girin. " & tipHata.Message)
+        Catch hata As Exception
+            MsgBox("Bilinmeyen hata. " & hata.Message)
+        End Try
 
-        sayi2 = CInt(TextBox2.Text)
     End Sub
 
 
