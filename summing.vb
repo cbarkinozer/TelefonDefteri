ac80de0c20c8613ed41a40f7126a357239c515e0
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-20 14:01:42 +0300

    Create summing.vb

---
+++ summing.vb
@@ -0,0 +1,26 @@
+Public Class Form1
+    Dim sayi1 As Integer
+    Dim sayi2 As Integer
+    Dim sayi3 As Integer
+
+    Private Sub Topla_Click(sender As Object, e As EventArgs) Handles Topla.Click
+        Try
+            sayi3 = sayi1 + sayi2
+            TextBox3.Text = sayi3
+        Catch tipHatasi As System.InvalidCastException
+            TextBox3.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        sayi1 = CInt(TextBox1.Text)
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+        sayi2 = CInt(TextBox2.Text)
+    End Sub
+
+
+End Class
