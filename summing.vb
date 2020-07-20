a3f1171e75366faad2249c895a30cd130584e03c
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-20 15:41:05 +0300

    Update summing.vb

--- summing.vb
+++ summing.vb
@@ -9,6 +9,22 @@ Public Class Form1
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        If TextBox1.Text.Contains(",") Then
+            Try
+
+                sayi1 = CSng(TextBox1.Text)
+
+
+            Catch sinirHata As IndexOutOfRangeException
+
+                sayi1 = CDbl(TextBox1.Text)
+            End Try
+            GoTo son
+        End If
+
+
+
         Try
             sayi1 = CInt(TextBox1.Text)
         Catch tipHata As System.InvalidCastException
@@ -16,6 +32,7 @@ Public Class Form1
         Catch hata As Exception
             MsgBox("Bilinmeyen hata. " & hata.Message)
         End Try
+son:
     End Sub
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
