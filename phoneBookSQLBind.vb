9a62bf28c786f85a454b1cd87dd1147139a25bac
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-22 15:27:36 +0300

    Update phoneBookSQLBind.vb

--- phoneBookSQLBind.vb
+++ phoneBookSQLBind.vb
@@ -1,5 +1,7 @@
 Imports System.Data.SqlClient
 Public Class Form1
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
     Dim connection As New SqlConnection("Server=DESKTOP-E5T285L;Database= telefonRehberi;Integrated Security =true")
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'TelefonRehberiDataSet.telefonDefteri' table. You can move, or remove it, as needed.
@@ -14,7 +16,15 @@ Public Class Form1
         command.ExecuteNonQuery()
         connection.Close()
     End Sub
-
+    Public Sub displayData()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         'ekle butonu
         Dim insertQuery As String = "INSERT INTO telefonDefteri (ad,soyad,eposta,yas,postaKodu,okulNo) " _
@@ -51,4 +61,30 @@ Public Class Form1
         ' ExecuteQuery(deleteQuery)
         'MessageBox.Show("Kişi silindi")
     End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+        i = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataReader(cmd)
+        da.Fill(dt)
+        Dim dr As SqlClient.SqlDataReader
+        dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+        While dr.Read
+            TextBox1.Text = dr.GetString(1).ToString()
+            TextBox2.Text = dr.GetString(2).ToString()
+            TextBox3.Text = dr.GetString(3).ToString()
+            TextBox4.Text = dr.GetString(4).ToString()
+            TextBox5.Text = dr.GetString(5).ToString()
+            TextBox6.Text = dr.GetString(6).ToString()
+        End While
+
+    End Sub
+
 End Class
