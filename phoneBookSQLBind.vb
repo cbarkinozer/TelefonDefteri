62a1d6ac6ab22e697dd87d53f804d52c11ec2f0b
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-23 10:58:16 +0300

    Update phoneBookSQLBind.vb

--- phoneBookSQLBind.vb
+++ phoneBookSQLBind.vb
@@ -3,11 +3,26 @@ Public Class Form1
     Dim con As New SqlConnection("Server=DESKTOP-E5T285L;Database= telefonRehberi;Integrated Security =true")
     Dim cmd As New SqlCommand
     Dim i As Integer
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'TelefonRehberiDataSet.telefonDefteri' table. You can move, or remove it, as needed.
         Me.TelefonDefteriTableAdapter.Fill(Me.TelefonRehberiDataSet.telefonDefteri)
 
 
+    End Sub
+    Private Sub loadData()
+        Try
+            Dim Str As String = "SELECT * FROM tbl_info"
+            con.Open()
+            Dim Search As New SqlDataAdapter(Str, con)
+            Dim ds As DataSet = New DataSet
+            Search.Fill(ds, "tbl_info")
+            DataGridView1.DataSource = ds.Tables("tbl_info")
+            con.Close()
+        Catch ex As Exception
+            MsgBox("loading data error" & ex.Message)
+            con.Close()
+        End Try
     End Sub
     Public Sub ExecuteQuery(query As String)
         Dim command As New SqlCommand(query, con)
@@ -17,55 +32,89 @@ Public Class Form1
         con.Close()
     End Sub
     Public Sub displayData()
-        cmd = con.CreateCommand()
-        cmd.CommandType = CommandType.Text
-        cmd.ExecuteNonQuery()
-        Dim dt As New DataTable()
-        Dim da As New SqlDataAdapter(cmd)
-        da.Fill(dt)
-        DataGridView1.DataSource = dt
+        Try
+            cmd = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox("ldisplay data error" & ex.Message)
+        End Try
+
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         'ekle butonu
-        Dim insertQuery As String = "INSERT INTO telefonDefteri (ad,soyad,eposta,yas,postaKodu,okulNo) " _
-        & " VALUES " & "('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "'," _
-        & TextBox4.Text & ",'" & TextBox5.Text & "','" & TextBox6.Text & "')"
-        ExecuteQuery(insertQuery)
-        MessageBox.Show("Kişi eklendi")
-        Dim X As Control
-        For Each X In Me.Controls
-            If TypeOf X Is TextBox Then
-                X.Text = ""
-            End If
-        Next X
+        Try
+            Dim insertQuery As String = "INSERT INTO telefonDefteri (ad,soyad,eposta,yas,postaKodu,okulNo) " _
+            & " VALUES " & "('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "'," _
+            & TextBox4.Text & ",'" & TextBox5.Text & "','" & TextBox6.Text & "')"
+
+            ExecuteQuery(insertQuery)
+
+
+
+            loadData()
+
+
+            MessageBox.Show("Kişi eklendi")
+
+
+
+            Dim X As Control
+            For Each X In Me.Controls
+                If TypeOf X Is TextBox Then
+                    X.Text = ""
+                End If
+            Next X
+        Catch ex As Exception
+            MsgBox("adding error" & ex.Message)
+        End Try
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'güncelle butonu
-        If con.State = ConnectionState.Open Then
-            con.Close()
-        End If
-        con.Open()
-        cmd = con.CreateCommand()
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = " UPDATE telefonDefteri SET ad ='" + TextBox1.Text + "' ,soyad= '" + TextBox2.Text + "' ,eposta='" + TextBox3.Text +
-        "' ,yas='" + TextBox4.Text + "' ,postaKodu='" + TextBox5.Text + "' ,okulNo='" + TextBox6.Text + "'  where id= " & i & "  "
-        cmd.ExecuteNonQuery()
-        displayData()
-        MessageBox.Show("Kişiler güncellendi")
-        Dim X As Control
-        For Each X In Me.Controls
-            If TypeOf X Is TextBox Then
-                X.Text = ""
-            End If
-        Next X
+        Try
+            Dim updateQuery As String = " UPDATE telefonDefteri SET ad ='" + TextBox1.Text + "' ,soyad= '" + TextBox2.Text + "' ,eposta='" + TextBox3.Text +
+            "' ,yas='" + TextBox4.Text + "' ,postaKodu='" + TextBox5.Text + "' ,okulNo='" + TextBox6.Text + "'  where telefonDefteriID= " & i & ""
+            ExecuteQuery(updateQuery)
+            displayData()
+            MessageBox.Show("Kişiler güncellendi")
+            Dim X As Control
+            For Each X In Me.Controls
+                If TypeOf X Is TextBox Then
+                    X.Text = ""
+                End If
+            Next X
+
+
+        Catch ex As Exception
+            MsgBox("update error" & ex.Message)
+        End Try
+
+
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         'sil butonu
-        'Dim deleteQuery As String = " "
-        ' ExecuteQuery(deleteQuery)
-        'MessageBox.Show("Kişi silindi")
+        Try
+            If DataGridView1.SelectedRows.Count > 0 Then
+                Dim deleteQuery As String = "DELETE FROM telefonDefteri WHERE telefonDefteriID= " & i & ""
+                ExecuteQuery(deleteQuery)
+            Else
+                MessageBox.Show("No rows to select")
+            End If
+            MessageBox.Show("Kişi silindi")
+
+        Catch ex As Exception
+            MsgBox("delete error" & ex.Message)
+        End Try
+
+
     End Sub
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
@@ -75,7 +124,7 @@ Public Class Form1
             End If
             con.Open()
 
-            i = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+            i = DataGridView1.SelectedCells.Item(0).Value
             cmd = con.CreateCommand()
             cmd.CommandType = CommandType.Text
             cmd.ExecuteNonQuery()
