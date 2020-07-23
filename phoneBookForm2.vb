9690b74894334cb293c047e63e4d12e3ae6f15c5
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-23 14:49:20 +0300

    Update phoneBookForm2.vb

--- phoneBookForm2.vb
+++ phoneBookForm2.vb
@@ -9,6 +9,7 @@ Public Class Form2
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'ekle
         Try
+            Button2.Visible = False
             Dim insertQuery As String = "INSERT INTO telefonDefteri (ad,soyad,eposta,yas,postaKodu,okulNo) " _
             & " VALUES " & "('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "'," _
             & TextBox4.Text & ",'" & TextBox5.Text & "','" & TextBox6.Text & "')"
@@ -33,6 +34,7 @@ Public Class Form2
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         'güncelle
         Try
+            Button1.Visible = False
             Form1.displayData()
             i = Form1.DataGridView1.SelectedCells.Item(0).Value
             Dim updateQuery As String = " UPDATE telefonDefteri SET ad ='" + TextBox1.Text + "' ,soyad= '" + TextBox2.Text + "' ,eposta='" + TextBox3.Text +
