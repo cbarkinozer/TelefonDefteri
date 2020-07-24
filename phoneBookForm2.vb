099a24e9cf730eccd549f55458f44df0fead19ac
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2020-07-24 09:29:30 +0300

    Update phoneBookForm2.vb

--- phoneBookForm2.vb
+++ phoneBookForm2.vb
@@ -1,62 +1,99 @@
+Imports System.Data.SqlClient
 Public Class Form2
+    Dim con As New SqlConnection("Server=DESKTOP-E5T285L;Database= telefonRehberi;Integrated Security =true")
+    Dim cmd As New SqlCommand
     Dim i As Integer
 
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'ekle
-        Try
-            Button2.Visible = False
-            Dim insertQuery As String = "INSERT INTO telefonDefteri (ad,soyad,eposta,yas,postaKodu,okulNo) " _
-            & " VALUES " & "('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "'," _
-            & TextBox4.Text & ",'" & TextBox5.Text & "','" & TextBox6.Text & "')"
+        'kaydet
 
-            Form1.ExecuteQuery(insertQuery)
 
-            Form1.loadData()
+        If (Tag = 0) Then
+            'güncelle
+            Try
+                Form1.displayData() 'show selected row
+                i = Form1.DataGridView1.SelectedRows.Item(0).ToString 'get selected row
 
-            MessageBox.Show("Kişi eklendi")
+                If i = "" Then ' be sure its not empty
+                    MessageBox.Show("Empty Id")
 
-            Dim X As Control
-            For Each X In Me.Controls
-                If TypeOf X Is TextBox Then
-                    X.Text = ""
+                Else
+                    'sql command
+                    Dim command As New SqlCommand("UPDATE Users SET ad = @name ,soyad = @surname ,eposta = @email, _
+                    yas=@age,postaKodu=@pcode,okulNo=@sno WHERE telefonDefteriID = @id", con)
+
+                    'sql injection security parameters
+                    command.Parameters.Add("@name", SqlDbType.VarChar).Value = TextBox1.Text
+                    command.Parameters.Add("@surname", SqlDbType.VarChar).Value = TextBox2.Text
+                    command.Parameters.Add("@email", SqlDbType.VarChar).Value = TextBox3.Text
+                    command.Parameters.Add("@age", SqlDbType.Int).Value = TextBox4.Text
+                    command.Parameters.Add("@pcode", SqlDbType.VarChar).Value = TextBox5.Text
+                    command.Parameters.Add("@sno", SqlDbType.VarChar).Value = TextBox6.Text
+
+                    con.Open()
+
+                    If command.ExecuteNonQuery() = 1 Then
+                        MessageBox.Show("Data Updated")
+
+                    Else
+                        MessageBox.Show("Data Not Updated")
+                    End If
+
+                    con.Close()
+
                 End If
-            Next X
-        Catch ex As Exception
-            MsgBox("adding error" & ex.Message)
-        End Try
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        'güncelle
-        Try
-            Button1.Visible = False
-            Form1.displayData()
-            i = Form1.DataGridView1.SelectedCells.Item(0).Value
-            Dim updateQuery As String = " UPDATE telefonDefteri SET ad ='" + TextBox1.Text + "' ,soyad= '" + TextBox2.Text + "' ,eposta='" + TextBox3.Text +
-            "' ,yas='" + TextBox4.Text + "' ,postaKodu='" + TextBox5.Text + "' ,okulNo='" + TextBox6.Text + "'  where telefonDefteriID= " & i & ""
-
-            Form1.ExecuteQuery(updateQuery)
-
-            Form1.loadData()
-
-            MessageBox.Show("Kişiler güncellendi")
-
-            Dim X As Control
-            For Each X In Me.Controls
-                If TypeOf X Is TextBox Then
-                    X.Text = ""
-                End If
-            Next X
 
 
-        Catch ex As Exception
-            MsgBox("update error" & ex.Message)
-        End Try
+
+                Form1.loadData()
+
+                MessageBox.Show("Kişiler güncellendi")
+
+                Dim X As Control
+                For Each X In Me.Controls
+                    If TypeOf X Is TextBox Then
+                        X.Text = ""
+                    End If
+                Next X
+
+            Catch ex As Exception
+                MsgBox("update error" & ex.Message)
+            End Try
+
+
+
+        ElseIf (Tag = 1) Then
+            'ekle
+            Try
+
+                Dim insertQuery As String = "INSERT INTO telefonDefteri (ad,soyad,eposta,yas,postaKodu,okulNo) " _
+                & " VALUES " & "('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "'," _
+                & TextBox4.Text & ",'" & TextBox5.Text & "','" & TextBox6.Text & "')"
+
+                Form1.ExecuteQuery(insertQuery)
+
+                Form1.loadData()
+
+                MessageBox.Show("Kişi eklendi")
+
+                Dim X As Control
+                For Each X In Me.Controls
+                    If TypeOf X Is TextBox Then
+                        X.Text = ""
+                    End If
+                Next X
+
+            Catch ex As Exception
+                MsgBox("adding error" & ex.Message)
+            End Try
+
+        End If
 
     End Sub
 End Class
